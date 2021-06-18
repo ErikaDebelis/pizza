b15ba4474dbd9dd4c91df348aee5a15a1ce575f6
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-18 08:54:27 -0700

    create mindmap(in images for ref) to decide + begin logic

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# __We Come in Piezza__
+# __Pizza Planet__
 ## co-authored by Erika Debelis
 
 ### __Description__
-'We come in Piezza' is a website for a greek-style pizza restaurant. The customer builds their pizza.
+'Pizza Planet' is a website for a greek-style pizza restaurant. The customer builds their pizza.
--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,20 @@
 
 
 
-// Business Logic
-
+// Business Logic for objects
+function PizzaPlanet () {
+  this.pizza = {};
+  this.currentID = 0; //needed? i think so- need to keep track of pizzas 'added to cart'
+}
+function Pizza (size, toppings) {
+  this.chosenSize= size;
+  this.chosenToppings= toppings;
+}
+//Business logic for pizza
+PizzaPlanet.prototype.addPizza = function (pizza) {
+  pizza.id = this.assignPizzaId();
+  this.pizzas[pizza.id] = pizza;
+}
 
 
 
